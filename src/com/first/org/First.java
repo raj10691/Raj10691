0807404b479d954b6407a4720e64edbce7c06940
visalvani <vs.visalakshi@gmail.com>
2019-03-16 12:50:26 +0530

    my update

--- src/com/first/org/First.java
+++ src/com/first/org/First.java
@@ -1,8 +1,23 @@
 package com.first.org;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class First {
 	public static void main(String[] args) {
-		System.out.println("Created succesfully GIT");
+		System.setProperty("webdriver.chrome.driver", "C:\\New\\NewPrjct\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		WebElement nm = driver.findElement(By.id("email"));
+		nm.sendKeys("rkrajrock@gmail.com");
+		WebElement ps = driver.findElement(By.id("pass"));
+		ps.sendKeys("4321425354345");
+		
+		
+		
+		
 	}
 
 }
